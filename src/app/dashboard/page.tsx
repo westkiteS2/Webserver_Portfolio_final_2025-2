592a9664d19062237ea3feba49212b70f70970f0
westkiteS2 <seoyeon8513@gmail.com>
2025-11-24 15:20:30 +0900

    1st

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-5">Dashboard</h1>
+      <p className="mb-5">Welcome to Dashboard.</p>
+      <p className="mb-5">
+        This page shows the perconal information of the logged-in user.
+      </p>
+    </div>
+  )
+}
+//로그인되지 않아도 보이는 페이지 
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome Home!</h1>
+    </div>
+  )
+}
